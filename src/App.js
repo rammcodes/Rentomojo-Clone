f7d321e1b79722881efc3e1b936e971b90980218
Ram Maheshwari <ram991011@gmail.com>
2020-05-17 21:58:44 +0530

    started working on homeview header

--- src/App.js
+++ src/App.js
@@ -30,6 +30,93 @@ class App extends React.Component {
             <button className="nav-btn primary-btn">Login / Signup</button>
           </div>
         </div>
+        <header className="homeview-header">
+          <div className="hero-showcase">
+            <div className="hero-cont">
+              <img
+                className="hero"
+                src="https://s.rmjo.in/2020/4/1/Take-the-leap-9u5t0h.jpg"
+                alt="hero"
+              />
+            </div>
+          </div>
+          <div className="category">
+            <div className="container">
+              <div className="category-list">
+                <div className="cat">
+                  <div className="cat-icon-cont">
+                    <img
+                      className="icon"
+                      src="https://rentomojo.com/public/images/categories/new-furniture.svg"
+                      alt="cat"
+                    />
+                  </div>
+                  <h4 className="cat-name">Electronics</h4>
+                </div>
+                <div className="cat">
+                  <div className="cat-icon-cont">
+                    <img
+                      className="icon"
+                      src="https://rentomojo.com/public/images/categories/new-furniture.svg"
+                      alt="cat"
+                    />
+                  </div>
+                  <h4 className="cat-name">Electronics</h4>
+                </div>
+                <div className="cat">
+                  <div className="cat-icon-cont">
+                    <img
+                      className="icon"
+                      src="https://rentomojo.com/public/images/categories/new-furniture.svg"
+                      alt="cat"
+                    />
+                  </div>
+                  <h4 className="cat-name">Electronics</h4>
+                </div>
+                <div className="cat">
+                  <div className="cat-icon-cont">
+                    <img
+                      className="icon"
+                      src="https://rentomojo.com/public/images/categories/new-furniture.svg"
+                      alt="cat"
+                    />
+                  </div>
+                  <h4 className="cat-name">Electronics</h4>
+                </div>
+                <div className="cat">
+                  <div className="cat-icon-cont">
+                    <img
+                      className="icon"
+                      src="https://rentomojo.com/public/images/categories/new-furniture.svg"
+                      alt="cat"
+                    />
+                  </div>
+                  <h4 className="cat-name">Electronics</h4>
+                </div>
+                <div className="cat">
+                  <div className="cat-icon-cont">
+                    <img
+                      className="icon"
+                      src="https://rentomojo.com/public/images/categories/new-furniture.svg"
+                      alt="cat"
+                    />
+                  </div>
+                  <h4 className="cat-name">Electronics</h4>
+                </div>
+                <div className="cat">
+                  <div className="cat-icon-cont">
+                    <img
+                      className="icon"
+                      src="https://rentomojo.com/public/images/categories/new-furniture.svg"
+                      alt="cat"
+                    />
+                  </div>
+                  <h4 className="cat-name">Electronics</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
       </div>
     )
   }
